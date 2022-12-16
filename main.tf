e934cf5a5bd1297c5bb1ba3f5f04ee4494a6b9d4
Khanh Nguyen <khanhntd@amazon.com>
2022-12-16 15:08:40 -0500

    Update default resources

--- main.tf
+++ main.tf
@@ -5,6 +5,15 @@ resource "random_id" "testing_id" {
   byte_length = 8
 }
 
+##########################################
+# Template Files
+##########################################
+
+locals {
+  cwagent_config         = fileexists("./resources/amazon_cloudwatch_agent.json")
+  cwagent_ecs_taskdef    = fileexists("./resources/statsd.sh")
+}
+
 #####################################################################
 # Generate EC2 Key Pair for log in access to EC2
 #####################################################################
@@ -22,8 +31,8 @@ resource "aws_key_pair" "aws_ssh_key" {
 }
 
 locals {
-  ssh_key_name        = var.ssh_key_name != "" ? var.ssh_key_name : aws_key_pair.aws_ssh_key[0].key_name
-  private_key_content = var.ssh_key_name != "" ? var.ssh_key_value : tls_private_key.ssh_key[0].private_key_pem
+  ssh_key_name        = aws_key_pair.aws_ssh_key[0].key_name
+  private_key_content = tls_private_key.ssh_key[0].private_key_pem
 }
 
 #####################################################################
@@ -38,7 +47,7 @@ resource "aws_instance" "cwagent" {
   associate_public_ip_address = true
 
   tags = {
-    Name = "cwagent-integ-test-ec2-${var.test_name}-${random_id.testing_id.hex}"
+    Name = "cwagent-integ-test-ec2-${random_id.testing_id.hex}"
   }
 }
 
@@ -46,23 +55,8 @@ resource "null_resource" "integration_test" {
   # Prepare Integration Test
   provisioner "remote-exec" {
     inline = [
-      "echo sha ${var.cwa_github_sha}",
-      "cloud-init status --wait",
-      "echo clone and install agent",
-      "git clone ${var.github_test_repo}",
-      "cd amazon-cloudwatch-agent-test",
-      "git reset --hard ${var.cwa_test_github_sha}",
-      "aws s3 cp s3://${var.s3_bucket}/integration-test/binary/${var.cwa_github_sha}/linux/${var.arc}/${var.binary_name} .",
-      "sleep 10",
-      "export PATH=$PATH:/snap/bin:/usr/local/go/bin",
-      var.install_agent,
-      "echo get ssl pem for localstack and export local stack host name",
-      "cd ~/amazon-cloudwatch-agent-test/localstack/ls_tmp",
-      "aws s3 cp s3://${var.s3_bucket}/integration-test/ls_tmp/${var.cwa_github_sha} . --recursive",
-      "cat ${var.ca_cert_path} > original.pem",
-      "cat original.pem snakeoil.pem > combine.pem",
-      "sudo cp original.pem /opt/aws/amazon-cloudwatch-agent/original.pem",
-      "sudo cp combine.pem /opt/aws/amazon-cloudwatch-agent/combine.pem",
+        "sudo rpm -Uvh https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm",
+
     ]
 
     connection {
@@ -76,21 +70,11 @@ resource "null_resource" "integration_test" {
   #Run sanity check and integration test
   provisioner "remote-exec" {
     inline = [
-      "echo prepare environment",
-      "export LOCAL_STACK_HOST_NAME=${var.local_stack_host_name}",
-      "export AWS_REGION=${var.region}",
-      "export PATH=$PATH:/snap/bin:/usr/local/go/bin",
-      "echo run integration test",
-      "cd ~/amazon-cloudwatch-agent-test",
-      "echo run sanity test && go test ./test/sanity -p 1 -v --tags=integration",
-      "export SHA=${var.cwa_github_sha}",
-      "export SHA_DATE=${var.cwa_github_sha_date}",
-      "export PERFORMANCE_NUMBER_OF_LOGS=${var.performance_number_of_logs}",
-      "go test ${var.test_dir} -p 1 -timeout 30m -computeType=EC2 -v --tags=integration "
+
     ]
     connection {
       type        = "ssh"
-      user        = var.user
+      user        = "ec2-user"
       private_key = local.private_key_content
       host        = aws_instance.cwagent.public_ip
     }
@@ -105,7 +89,6 @@ data "aws_ami" "latest" {
 
   filter {
     name   = "name"
-    values = [var.ami]
+    values = ["cloudwatch-agent-integration-test-al2*"]
   }
 }
-Footer
