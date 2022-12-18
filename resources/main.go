0b9a4baea5dd3b373b59d336eb333586066890f4
Khanh Nguyen <khanhntd@amazon.com>
2022-12-17 22:04:06 -0500

    Delete and merge main.go

--- resources/main.go
+++ resources/main.go
@@ -4,16 +4,26 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"strings"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/cloudwatch"
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatch/types"
 	"github.com/cactus/go-statsd-client/v5/statsd"
 )
 
 const (
 	RetryTime = 15
+)
+
+var (
+	ctx context.Context
+	cwm *cloudwatch.Client
 )
 
 var threshold = map[int]float64{
@@ -74,4 +84,66 @@ func sendStatsDStress(tps int) error {
 		client.Inc(fmt.Sprintf("statsd_%v", time), int64(time), 1.0)
 	}
 	return nil
+}
+
+func GetCloudWatchMetricsClient() (*cloudwatch.Client, *context.Context, error) {
+	if cwm == nil {
+		ctx = context.Background()
+		c, err := config.LoadDefaultConfig(ctx)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		cwm = cloudwatch.NewFromConfig(c)
+	}
+	return cwm, &ctx, nil
+}
+
+func GetMetricDataResults(namespace, metricName string, metricSpecificDimensions []types.Dimension) ([]float64, error) {
+	metricToFetch := types.Metric{
+		Namespace:  aws.String(namespace),
+		MetricName: aws.String(metricName),
+		Dimensions: metricSpecificDimensions,
+	}
+
+	metricQueryPeriod := int32(60)
+	metricDataQueries := []types.MetricDataQuery{
+		{
+			MetricStat: &types.MetricStat{
+				Metric: &metricToFetch,
+				Period: &metricQueryPeriod,
+				Stat:   aws.String("Average"),
+			},
+			Id: aws.String(strings.ToLower(metricName)),
+		},
+	}
+
+	endTime := time.Now()
+	startTime := subtractMinutes(endTime, 10)
+	getMetricDataInput := cloudwatch.GetMetricDataInput{
+		StartTime:         &startTime,
+		EndTime:           &endTime,
+		MetricDataQueries: metricDataQueries,
+	}
+
+	log.Printf("Metric data input is : %s", fmt.Sprint(getMetricDataInput))
+
+	cwmClient, clientContext, err := GetCloudWatchMetricsClient()
+	if err != nil {
+		return nil, fmt.Errorf("Error occurred while creating CloudWatch client: %v", err.Error())
+	}
+
+	output, err := cwmClient.GetMetricData(*clientContext, &getMetricDataInput)
+	if err != nil {
+		return nil, fmt.Errorf("Error getting metric data %v", err)
+	}
+
+	result := output.MetricDataResults[0].Values
+	log.Printf("Metric values are : %s", fmt.Sprint(result))
+	return result, nil
+}
+
+func subtractMinutes(fromTime time.Time, minutes int) time.Time {
+	tenMinutes := time.Duration(-1*minutes) * time.Minute
+	return fromTime.Add(tenMinutes)
 }
--- resources/statsd.sh
+++
@@ -1,8 +0,0 @@
-for times in  {1..3}
-do
-        for ti in  {1..1000}
-        do
-            echo "statsd.counter.${ti}:1|c" | nc -w 1 -u 127.0.0.1 8125
-        done
-        sleep 60
-done
