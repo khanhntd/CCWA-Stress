a2609aee12bcced8060a2dacbebe566eb163758c
Khanh Nguyen <khanhntd@amazon.com>
2022-12-22 10:10:12 -0500

    Add pprof

--- pprof.go
+++ pprof.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	_ "net/http/pprof"
 )
 
 func init() {
 	go func() {
-		http.ListenAndServe(":1234", nil)
+		if err := http.ListenAndServe(":1234", nil); err != nil {
+			log.Fatal("E! " + err.Error())
+		}
 	}()
 }
